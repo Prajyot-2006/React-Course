6e8138d9da806d57b9c78cfa25b044edc930b41e
Prajyot Jore <prajyotjore06@gmail.com>
2025-08-27 22:36:49 +0530

    Updated App.jsx, removed old tracking files, added new TrackingPage

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
 import { Routes , Route } from 'react-router'  // we gonna use routes thats why we are loading it
 import { Orders } from './pages/OrdersPage'
+import { Tracking } from './pages/TrackingPage'
 import './App.css'
 
 
@@ -18,6 +19,7 @@ function App() {
       <Route path="/" element={<HomePage />}></Route>  {/*we gonna give this Route component 2 props 1st prop is path (this tells react the URL path of the page) for the homepage the url path is empty so thast why we gave / to path.  2nd prop is element , this tells react which element or component to display*/}
       <Route path="checkout" element={<CheckoutPage />}></Route>
       <Route path="orders" element={<Orders />}></Route>
+      <Route path="tracking" element={<Tracking />}></Route>
     </Routes>
   )
 }
@@ -25,3 +27,6 @@ function App() {
 export default App
 
 // routng is actually pretty simple , we just tell react which pages we want in our website and then waht to display for each page
+
+
+// in lesson 6 we learned
